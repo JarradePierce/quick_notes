a84de249da5472da50b220f202b389d4627a342d
Jarrade Pierce <x-jpierce@403-laptop.corp.zooxlabs.com>
2020-01-09 16:20:11 -0800

    setting up the notes index page

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,28 @@
 class NotesController < ApplicationController
 
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+  end
+
   def index
     @notes = Note.all
+  end
+
+  def show
+    @note = find_note
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content, :title, :comment)
+  end
+
+  def find_note
+    @note = Note.find(params[:id])
   end
 end
